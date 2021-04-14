94e870b43b0ca821eccc615a67a0c997a3500d8c
NoronhaGCP <inc.691@gmail.com>
2021-04-14 20:59:49 -0300

    start formulario_activity with extra props

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.agenda;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,7 +12,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.agenda.DAO.AlunoDAO;
+import com.example.agenda.model.Aluno;
 import com.example.agenda.ui.activity.FormularioAlunoActivity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,12 +33,23 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         AlunoDAO dao = new AlunoDAO();
+
+        dao.salva(new Aluno("Noronha", "dasniodnaso", "odasnmdoa"));
+        dao.salva(new Aluno("Froid", "dasniodnaso", "odasnmdoa"));
+
+        final List<Aluno> alunos = dao.getAlunos();
+
         ListView listaAlunos = findViewById(R.id.activity_lista_alunos_listview);
 
         listaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.getAlunos()));
 
         listaAlunos.setOnItemClickListener((AdapterView<?> adapterView, View view, int index, long id) -> {
+            Aluno aluno = alunos.get(index);
 
+            Intent vaiParaFormulario = new Intent(MainActivity.this, FormularioAlunoActivity.class);
+            vaiParaFormulario.putExtra("aluno", aluno);
+
+            startActivity(vaiParaFormulario);
         });
     }
 
