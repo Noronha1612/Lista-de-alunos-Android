8dde4215689ee5d981440acbcfd6231b3bc0a85e
NoronhaGCP <inc.691@gmail.com>
2021-04-14 22:10:23 -0300

    create update method

--- app/src/main/java/com/example/agenda/DAO/AlunoDAO.java
+++ app/src/main/java/com/example/agenda/DAO/AlunoDAO.java
@@ -1,5 +1,7 @@
 package com.example.agenda.DAO;
 
+import android.util.Log;
+
 import com.example.agenda.model.Aluno;
 
 import java.util.ArrayList;
@@ -7,12 +9,34 @@ import java.util.List;
 
 public class AlunoDAO {
     private final static List<Aluno> alunos = new ArrayList<>();
+    private static int contadorDeIds = 1;
 
     public void salva(Aluno aluno) {
         alunos.add(aluno);
+        contadorDeIds++;
+    }
+
+    public void edita(Aluno aluno) {
+        Aluno alunoEncontrado = null;
+
+        for (Aluno a: alunos) {
+            if ( a.getId() == aluno.getId() ) {
+                alunoEncontrado = a;
+            }
+        }
+
+        if ( alunoEncontrado != null ) {
+            int posicaoDoAluno = alunos.indexOf(alunoEncontrado);
+
+            alunos.set(posicaoDoAluno, aluno);
+        }
     }
 
     public List<Aluno> getAlunos() {
         return new ArrayList<>(alunos);
+    }
+
+    public static int getContadorDeIds() {
+        return contadorDeIds;
     }
 }
