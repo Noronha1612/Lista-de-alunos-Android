9746e6e73c9a509ec7430c10103db39bd8414d65
NoronhaGCP <inc.691@gmail.com>
2021-04-14 21:29:34 -0300

    implement initial value to name field by extra prop

--- app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -10,24 +10,38 @@ import com.example.agenda.DAO.AlunoDAO;
 import com.example.agenda.R;
 import com.example.agenda.model.Aluno;
 
+import java.io.Serializable;
+
 public class FormularioAlunoActivity extends AppCompatActivity {
 
-    public static final String NOVO_ALUNO = "Novo aluno";
+    private static final String NOVO_ALUNO = "Novo aluno";
+    private EditText campoNome;
+    private EditText campoTelefone;
+    private EditText campoEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
         setTitle(NOVO_ALUNO);
+
+        iniciarCampos();
+
+        Aluno aluno = (Aluno) getIntent().getSerializableExtra("aluno");
+        if(aluno != null)
+            campoNome.setText(aluno.toString());
+    }
+
+    private void iniciarCampos() {
+        this.campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        this.campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        this.campoEmail = findViewById(R.id.activity_formulario_aluno_email);
     }
 
     public void handleSaveStudent(View view) {
-        String alunoNome = ((EditText) findViewById(R.id.activity_formulario_aluno_nome))
-                .getText().toString();
-        String alunoTelefone = ((EditText) findViewById(R.id.activity_formulario_aluno_telefone))
-                .getText().toString();
-        String alunoEmail = ((EditText) findViewById(R.id.activity_formulario_aluno_email))
-                .getText().toString();
+        String alunoNome = campoNome.getText().toString();
+        String alunoTelefone = campoTelefone.getText().toString();
+        String alunoEmail = campoEmail.getText().toString();
 
         Aluno aluno = new Aluno(alunoNome, alunoTelefone, alunoEmail);
 
