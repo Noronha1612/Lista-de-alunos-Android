94f6baa2e5b9dbd83592adaca8ebeb301a6a151b
NoronhaGCP <inc.691@gmail.com>
2021-04-14 22:09:56 -0300

    add id attribute

--- app/src/main/java/com/example/agenda/model/Aluno.java
+++ app/src/main/java/com/example/agenda/model/Aluno.java
@@ -2,6 +2,8 @@ package com.example.agenda.model;
 
 import androidx.annotation.NonNull;
 
+import com.example.agenda.DAO.AlunoDAO;
+
 import java.io.Serializable;
 
 public class Aluno implements Serializable {
@@ -9,10 +11,14 @@ public class Aluno implements Serializable {
     private final String alunoTelefone;
     private final String alunoEmail;
 
+    private int id = 0;
+
     public Aluno(String alunoNome, String alunoTelefone, String alunoEmail) {
         this.alunoNome = alunoNome;
         this.alunoTelefone = alunoTelefone;
         this.alunoEmail = alunoEmail;
+
+        this.id = AlunoDAO.getContadorDeIds();
     }
 
     @NonNull
@@ -31,5 +37,13 @@ public class Aluno implements Serializable {
 
     public String getTelefone() {
         return alunoTelefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
