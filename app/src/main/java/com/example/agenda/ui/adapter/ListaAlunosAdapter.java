ea184a975bbf15d1b6e16880adcdd579f78d6641
NoronhaGCP <inc.691@gmail.com>
2021-04-15 22:02:18 -0300

    create custom adapter

---
+++ app/src/main/java/com/example/agenda/ui/adapter/ListaAlunosAdapter.java
@@ -0,0 +1,74 @@
+package com.example.agenda.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.agenda.R;
+import com.example.agenda.model.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaAlunosAdapter extends BaseAdapter {
+
+    private final List<Aluno> alunos = new ArrayList<>();
+    private final Context context;
+
+    public ListaAlunosAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return alunos.size();
+    }
+
+    @Override
+    public Aluno getItem(int position) {
+        return alunos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return alunos.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View viewCriada = criaView(parent);
+        Aluno alunoDevolvido = alunos.get(position);
+
+        vinculaInformacoes(viewCriada, alunoDevolvido);
+
+        return viewCriada;
+    }
+
+    private void vinculaInformacoes(View view, Aluno aluno) {
+        TextView nome = view.findViewById(R.id.item_aluno_nome);
+        TextView telefone = view.findViewById(R.id.item_aluno_telefone);
+
+        nome.setText(aluno.getNome());
+        telefone.setText(aluno.getTelefone());
+    }
+
+    private View criaView(ViewGroup parent) {
+        return LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_aluno, parent, false);
+    }
+
+    public void atualiza(List<Aluno> alunos) {
+        this.alunos.clear();
+        this.alunos.addAll(alunos);
+        notifyDataSetChanged();
+    }
+
+    public void remove(Aluno aluno) {
+        alunos.remove(aluno);
+        notifyDataSetChanged();
+    }
+}
